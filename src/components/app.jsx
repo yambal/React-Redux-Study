8b49f6e6110f7aa21b0c6ce83dbca0c97a177055
June YAMAMOTO <june@yambal.net>
2018-12-22 19:52:54 +0900

    axios を利用してAPIに問い合わせる

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,30 +1,75 @@
+import axios from 'axios';
 import React, { Component } from 'react';
-import Greeting from './greeting';
+import SearchForm from './SearchForm';
+import GeocodeResult from './GeocodeResult';
+
+const GEOCODE_ENDPOINT = 'https://maps.googleapis.com/maps/api/geocode/json';
+const GOOGLE_MAP_APIKEY = 'AIzaSyCINYzcjOFN4ChmBlhWaWOsKwkA4UQeHn4';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: 'Jhon',
+      address: '',
+      lat: 0,
+      lng: 0,
     };
   }
 
-  handleChangeName(name) {
+  setErrorMessage(message) {
     this.setState({
-      name,
+      address: message,
+      lat: 0,
+      lng: 0,
     });
+  }
+
+  handlePlaceSubmit(place) {
+    axios
+      .get(GEOCODE_ENDPOINT, {
+        params: {
+          key: GOOGLE_MAP_APIKEY,
+          address: place,
+        },
+      })
+      .then((results) => {
+        const data = results.data;
+        const result = data.results[0];
+        switch (data.status) {
+          case 'OK': {
+            const location = result.geometry.location;
+            this.setState({
+              address: result.formatted_address,
+              lat: location.lat,
+              lng: location.lng,
+            });
+            break;
+          }
+          case 'ZERO_RESULTS': {
+            this.setErrorMessage('結果が見つかりませんでした');
+            break;
+          }
+          default: {
+            this.setErrorMessage('結果が見つかりませんでした');
+          }
+        }
+      })
+      .catch((error) => {
+        // console.log(error);
+        this.setErrorMessage('通信に失敗しました');
+      });
   }
 
   render() {
     return (
       <div>
-        <input
-          type="text"
-          value={this.state.name}
-          onChange={e => this.handleChangeName(e.target.value)}
+        <h1>緯度経度検索</h1>
+        <SearchForm onSubmit={place => this.handlePlaceSubmit(place)} />
+        <GeocodeResult
+          address={this.state.address}
+          lat={this.state.lat}
+          lng={this.state.lng}
         />
-        <button onClick={() => this.handleChangeName('Bob')}>Bob</button>
-        <Greeting name={this.state.name} />
       </div>
     );
   }
