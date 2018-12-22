0e1b26eb0e993488f609f4f72827bdd7e30039a7
June YAMAMOTO <june@yambal.net>
2018-12-22 16:40:16 +0900

    stateを用いてcomponentに状態を持たせる

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,8 +2,34 @@ import React, { Component } from 'react';
 import Greeting from './greeting';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'Jhon',
+    };
+  }
+
+  handleMouseOver() {
+    this.setState({
+      name: 'Bob',
+    });
+  }
+
+  handleMouseOut() {
+    this.setState({
+      name: 'Jhon',
+    });
+  }
+
   render() {
-    return (<Greeting name="June" />);
+    return (
+      <div
+        onMouseOver={() => this.handleMouseOver()}
+        onMouseOut={() => this.handleMouseOut()}
+      >
+        <Greeting name={this.state.name} />
+      </div>
+    );
   }
 }
 
